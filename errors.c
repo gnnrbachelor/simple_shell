ec6f771ef12336c9faac34a018da6d02ab44d938
rkbrian <rkbriankong@gmail.com>
2021-04-13 20:07:30 +0000

    betty happy and main function fixed for global variable

--- errors.c
+++ errors.c
@@ -1,5 +1,9 @@
 #include "shell.h"
 
+/**
+ * no_file - fuction to write error message
+ * @cmd: input command that triggers error message
+ */
 void no_file(char *cmd)
 {
 	char *buffer = NULL;
--- shell.c
+++ shell.c
@@ -1,32 +1,24 @@
 #include "shell.h"
 /**
  * main - main function of shell program
- * @argc: argument count
- * @argv: argument array
- * @env: environmental variable
  * Return: 0
  */
 
-int main(int argc, char **argv, char **env)
+int main(void)
 {
 	char *buffer;
 	char **command_array;
 	size_t size;
 	ssize_t line_size;
 	int i;
-	(void)argv;
-	(void)argc;
 
 	buffer = NULL;
-
 	if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, "$ ", 2);
 
 	while ((line_size = getline(&buffer, &size, stdin)) < 1024)
 	{
-
 		signal(SIGINT, SIG_IGN);
-
 		if (line_size == EOF)
 		{
 			if (isatty(STDIN_FILENO))
@@ -34,14 +26,12 @@ int main(int argc, char **argv, char **env)
 			free(buffer);
 			exit(EXIT_SUCCESS);
 		}
-
-
 		command_array = tokenize(buffer);
 		execute(command_array, buffer);
 		if (_strcmp(command_array[0], "env") == 0)
-			for (i = 0; env[i] != NULL; i++)
+			for (i = 0; environ[i] != NULL; i++)
 			{
-				_puts(env[i]);
+				_puts(environ[i]);
 				_putchar('\n');
 			}
 		size = 0;
